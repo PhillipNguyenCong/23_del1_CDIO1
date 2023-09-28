5e21306ade800fa8bd9930b3ecbc8e7d358f7b80
ChristofferBuhl <124875038+ChristofferBuhl@users.noreply.github.com>
2023-09-28 14:24:32 +0200

    Added Win if you hit two 6 in a row

--- CDIO1/src/Dicegame.java
+++ CDIO1/src/Dicegame.java
@@ -8,8 +8,9 @@ class Dicegame{
         int sum = 0;
         int roll1 = 0;
         int roll2 = 0;
+        int isLastRoll2Sixes = 0;
 
-        var test = new Test();
+        //var test = new Test();
         //System.out.println(test.testThrow(1000));
         //System.out.println(test.alike(1000));
 
@@ -23,9 +24,8 @@ class Dicegame{
 
         
         player1.isTurn = true;      //Player1 starts
-
     
-            while(!game.checkWin(player1.score, player2.score, roll1, roll2)){
+            while(!game.checkWin(player1.score, player2.score, roll1, roll2) && isLastRoll2Sixes != 2){
                 roll1 = dice.getValue();
                 roll2 = dice.getValue();
                 sum += roll1 + roll2;
@@ -43,19 +43,37 @@ class Dicegame{
                             System.out.println("You got an extra turn");
                             player1.isTurn = true;
                             player2.isTurn = false;
+                            isLastRoll2Sixes = 0;
                         }
                         else if (game.checkEkstraturn(roll1, roll2, player1.score)){
-                            player2.score += sum;
+                            player1.score += sum;
                             System.out.println("Score: " + sum + "\nTotal score: " + player2.score + "\n");
                             player1.isTurn = true;
                             player2.isTurn = false;
                             System.out.println("You hit 2 alike \nYou got an extra turn");
+                            isLastRoll2Sixes = 0;
+                        }
+                        else if (game.checkTwoSixes(roll1, roll2)){
+                            player1.score += sum;
+                            System.out.println("Score: " + sum + "\nTotal score: " + player2.score + "\n");
+                            player1.isTurn = true;
+                            player2.isTurn = false;
+                            isLastRoll2Sixes++;
+                            if(isLastRoll2Sixes == 1 && player2.score < 40){
+                                System.out.println("You hit 2 Sixes \nYou got an extra turn. \nIf you hit 2 sixes again you win!");
+                            }
+                            else if(isLastRoll2Sixes == 2){
+                                System.out.println("You got 2 sixes again!!");
+                            }
+
+                        
                         }
                         else{
                             player1.score += sum;
                             System.out.println("Score: " + sum + "\nTotal score: " + player1.score + "\n");
                             player1.isTurn = false;
                             player2.isTurn = true;
+                            isLastRoll2Sixes = 0;
                         }
                         
                     }
@@ -76,6 +94,7 @@ class Dicegame{
                             System.out.println("You got an extra turn");
                             player1.isTurn = false;
                             player2.isTurn = true;
+                            isLastRoll2Sixes = 0;
                         }
                         else if (game.checkEkstraturn(roll1, roll2, player2.score)){
                             player2.score += sum;
@@ -83,13 +102,30 @@ class Dicegame{
                             player1.isTurn = false;
                             player2.isTurn = true;
                             System.out.println("You hit 2 alike \nYou got an extra turn");
+                            isLastRoll2Sixes = 0;
 
+                        }
+                        else if (game.checkTwoSixes(roll1, roll2)){
+                            player2.score += sum;
+                            System.out.println("Score: " + sum + "\nTotal score: " + player2.score + "\n");
+                            player1.isTurn = false;
+                            player2.isTurn = true;
+                            isLastRoll2Sixes++;
+                            if(isLastRoll2Sixes == 1 && player2.score < 40){
+                                System.out.println("You hit 2 Sixes \nYou got an extra turn. \nIf you hit 2 sixes again you win!");
+                            }
+                            else if(isLastRoll2Sixes == 2){
+                                System.out.println("You got 2 sixes again!!");
+                            }
+
+                        
                         }
                         else{
                         player2.score += sum;
                         System.out.println("Score: " + sum + "\nTotal score: " + player2.score + "\n");
                         player1.isTurn = true;
                         player2.isTurn = false;
+                        isLastRoll2Sixes = 0;
                         }
                     }
                     else{
@@ -101,11 +137,11 @@ class Dicegame{
             }
 
         
-            if(player1.score > 39 && roll1 !=1 && roll2 !=1){
-                System.out.println(player1.name + "You WON!!");
+            if(player1.score > 39 && roll1 !=1 && roll2 !=1 || isLastRoll2Sixes == 2){
+                System.out.println(player1.name + " You WON!!");
             }
-            else if (player2.score > 39 && roll1 !=1 && roll2 !=1){
-                System.out.println(player2.name + "You WON!!");
+            else if (player2.score > 39 && roll1 !=1 && roll2 !=1 || isLastRoll2Sixes == 2){
+                System.out.println(player2.name + " You WON!!");
             }
             System.out.println("Game Stats" + "\n" + player1.name + "'s score:" + player1.score + "\n" + player2.name + "'s score:" + player2.score);
     }
--- CDIO1/src/Game.java
+++ CDIO1/src/Game.java
@@ -17,12 +17,16 @@ class Game{
         return score1 > 39 && roll1 == roll2 && roll1 != 1 && roll2 != 1 || score2 > 39 && roll1 == roll2 && roll1 != 1 && roll2 != 1;
     }
 
+    public boolean checkTwoSixes(int roll1, int roll2){
+        return roll1 == roll2;
+    }
+
     public boolean checkTwo1s(int roll1, int roll2){
         return roll1 == 1 && roll2 == 1;
     }
 
     public boolean checkEkstraturn(int roll1, int roll2, int playerscore){
-        return roll1 == roll2 && roll1 != 1 && playerscore < 40;
+        return roll1 == roll2 && roll1 != 1 && playerscore < 40 && roll1 != 6;
     }
 
     public String play(){
